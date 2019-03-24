e2f35e5c7fbc0ee1249aff53d60966d97be07db9
Agne Subelyte <agne.subelyte@protonmail.com>
2019-03-24 16:06:43 -0400

    function plotting the confusion matrix

---
+++ confusion_matrix.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# =============================================================================
+# Confusion matrix plot
+# =============================================================================
+
+def confusion_matrix_plot(cm, cmap):
+    plt.clf()
+    plt.imshow(cm, cmap, interpolation='nearest')
+    classNames = ['Not Fraud','Fraud']
+    plt.title('Confusion Matrix')
+    plt.colorbar()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    tick_marks = np.arange(len(classNames))
+    plt.xticks(tick_marks, classNames, rotation=45)
+    plt.yticks(tick_marks, classNames)
+    thresh = cm.max() / 2.
+    s = [['TN','FP'], ['FN', 'TP']]
+    for i in range(2):
+        for j in range(2):
+            plt.text(j,i, str(s[i][j])+"\n"+str(cm[i][j]),
+            horizontalalignment="center",
+            color="white" if cm[i, j] > thresh else "black")
+    plt.show()
