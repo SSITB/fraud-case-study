7c1bba7e53befbbbde91922bca55db11f6796769
Agne Subelyte <agne.subelyte@protonmail.com>
2019-03-26 20:45:23 -0400

    Flask application

---
+++ final_web_app/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, redirect, render_template
+from flask_bootstrap import Bootstrap
+import pickle
+from mongo import update_db, pull_values
+app = Flask(__name__)
+Bootstrap(app)
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template("index.html")
+
+
+@app.route('/score', methods=['POST'])
+def score():
+   try:
+      with open('model.pkl', 'rb') as f:
+          model = pickle.load(f)
+      update_db(model)
+   except:
+      pass
+
+   low, medium, high = pull_values()
+
+   return render_template("score.html", low=low, medium=medium, high=high)
+
+if __name__ == '__main__':
+
+   app.run(host='0.0.0.0', port=8080, debug=True)
+
+
---
+++ final_web_app/mongo.py
@@ -0,0 +1,40 @@
+from class_features import Features
+from pymongo import MongoClient
+import pandas as pd
+
+
+def update_db(model):
+    
+    new_events = pd.DataFrame()
+    
+    for i in collection.find({'prob': {'$exists': False}}):
+        event =  pd.DataFrame.from_dict(i, orient='index').T
+        event['object_id'] = int(event['object_id'])
+        new_events = new_events.append(event)
+    
+    new_events = new_events.reset_index()
+    
+    features = Features()
+    X = features.features_clean(new_events)
+
+    X['prob'] = model.predict_proba(X)[:,1]
+    X['prob']=pd.Series(pd.cut(X['prob'], bins=[0.0, 0.3, 0.6, 1.0], 
+                             labels=['Low Risk', 'Medium Risk', 'High Risk']))
+    X['object_id'] = pd.DataFrame(new_events['object_id']).astype('object')
+    X_dict =  X.to_dict('records')
+    for i in X_dict:
+        collection.find_one_and_update({'object_id' :i['object_id']},
+                                        {"$set": {'prob':i['prob']}})
+
+    return
+    
+def pull_values():
+
+    return collection.count_documents({"prob":'Low Risk'}), \
+            collection.count_documents({"prob":'Medium Risk'}), \
+                collection.count_documents({"prob":'High Risk'})
+
+
+mongo_client=MongoClient()
+db = mongo_client['Fraud_Detection']
+collection = db['Events']
