5f3273fc0e2230f8ef612dcd6fc4fc778eb36f34
Agne Subelyte <agne.subelyte@protonmail.com>
2019-03-26 20:44:21 -0400

    saving final model in the pickle file

--- model_pickle.py
+++ model_pickle.py
@@ -4,7 +4,7 @@
 
 import pandas as pd
 from class_features import Features
-from class_models import Gdbr
+from sklearn.ensemble import GradientBoostingClassifier
 
 df=pd.read_json('data/data.json',convert_dates=['approx_payout_date','event_created',
                                                 'event_published','event_start',
@@ -13,8 +13,19 @@ df=pd.read_json('data/data.json',convert_dates=['approx_payout_date','event_crea
 import pickle
 if __name__ == '__main__':
     features = Features()
-    X, y = features.features_clean(df)
-    gdbr = Gdbr(X, y.values.ravel()).fit()
+    X = features.features_clean(df)
+    
+    #Target variable = 1 if the event is fraudulent
+    df['fraud'] = df['acct_type'].str.contains('fraud')
+    y = pd.get_dummies(df['fraud'],drop_first=True)
+    
+    gdbr = GradientBoostingClassifier(learning_rate=0.01,
+                                  max_depth=10,
+                                  n_estimators=1000,
+                                  min_samples_leaf=100,
+                                  max_features=4)
+    
+    gdbr.fit(X, y.values.ravel())
 
     with open('model.pkl', 'wb') as f:
         pickle.dump(gdbr, f)
